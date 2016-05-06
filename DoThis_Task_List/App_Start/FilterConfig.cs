59d02db8749d9a1742a27d23152bdc6bcbb63458
Margaret Landefeld <mlandefeld@users.noreply.github.com>
2016-05-05 20:21:48 -0400

    first commit

---
+++ DoThis_Task_List/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DoThis_Task_List
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ DoThis_Task_List/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DoThis_Task_List.MvcApplication" Language="C#" %>
